800a3e1c2c93f90ca6c7b23809a242887a4e8e23
Conor <conormccue@gmail.com>
2019-11-05 22:03:01 -0800

    solve challenge 3

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -38,9 +38,7 @@ CHALLENGE 3
 Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe function from challenge 2, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------ */
 
-const mapTwoToThe = (arr) => {
-  // Solution code here...
-};
+const mapTwoToThe = (arr) => arr.map( number => Math.pow(2, number));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
